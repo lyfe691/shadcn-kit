95042300cfce4e163344a3738a8e27c9ed894414
lyfe691 <yanis.sebastian.zuercher@gmail.com>
2025-07-03 15:16:54 +0200

    feat: theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button"
+import { ModeToggle } from "@/components/mode-toggle"
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="flex flex-wrap items-center gap-2 md:flex-row">
+      <Button variant="outline">Button</Button>
+      <ModeToggle />
     </div>
   )
 }
